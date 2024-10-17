e3a75bf43f76e1e252e09b7ef62e0fd8c2ba4bb8
Raherimalalanantenaina <168984775+Raherimalalanantenaina@users.noreply.github.com>
2024-10-17 09:58:18 +0200

    founisseurupdate
    hghjhgjhj

--- src/util/fournisseur/Fournisseur.java
+++ src/util/fournisseur/Fournisseur.java
@@ -1,28 +1,47 @@
 package util.fournisseur;
 
 public class Fournisseur {
-    private int id;
-    private String nom;
+    private int idFournisseur;
+    private String nomFournisseur;
+    private String email;
+    private String motDePasse;
 
-    public Fournisseur(int id, String nom) {
-        this.id = id;
-        this.nom = nom;
+    public Fournisseur(int idFournisseur, String nomFournisseur, String email, String motDePasse) {
+        this.idFournisseur = idFournisseur;
+        this.nomFournisseur = nomFournisseur;
+        this.email = email;
+        this.motDePasse = motDePasse;
     }
 
-    public int getId() {
-        return id;
+    public int getIdFournisseur() {
+        return idFournisseur;
     }
 
-    public void setId(int id) {
-        this.id = id;
+    public void setIdFournisseur(int idFournisseur) {
+        this.idFournisseur = idFournisseur;
     }
 
-    public String getNom() {
-        return nom;
+    public String getNomFournisseur() {
+        return nomFournisseur;
     }
 
-    public void setNom(String nom) {
-        this.nom = nom;
+    public void setNomFournisseur(String nomFournisseur) {
+        this.nomFournisseur = nomFournisseur;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getMotDePasse() {
+        return motDePasse;
+    }
+
+    public void setMotDePasse(String motDePasse) {
+        this.motDePasse = motDePasse;
     }
 }
-
