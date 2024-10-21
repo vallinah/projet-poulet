f21ea046e3cb4b8af651db47511483a98abe0776
Fifaliana_valea <159806408+fifaliana-valea@users.noreply.github.com>
2024-10-21 09:18:44 +0300

    correction

--- script.bat
+++ script.bat
@@ -13,68 +13,55 @@ set "web_apps=C:\Program Files\Apache Software Foundation\Tomcat 10.1\webapps"
 set "war_name=pouletFournisseur"
 set "war_name1=pouletAdmin"
 
-
 :: Effacer le dossier [temp]
 if exist "%temp%" (
     rd /s /q "%temp%"
 )
 
-:: Creation de la structure de dossier temporaire
+:: Création de la structure de dossier temporaire
 mkdir "%temp%\WEB-INF\lib"
 mkdir "%temp%\WEB-INF\classes"
+mkdir "%temp%\assets"
+mkdir "%temp%\forms"
 
-:: Effacer le dossier [temp]
+:: Effacer le dossier [temp1]
 if exist "%temp1%" (
     rd /s /q "%temp1%"
 )
 
-:: Creation de la structure de dossier temporaire
+:: Création de la structure de dossier temporaire
 mkdir "%temp1%\WEB-INF\lib"
 mkdir "%temp1%\WEB-INF\classes"
+mkdir "%temp1%\assets"
+mkdir "%temp1%\forms"
 
-
-:: Copie de web.xml ver temp/WEB-INF...
+:: Copie de web.xml vers temp/WEB-INF...
 xcopy /y "%web_xml%" "%temp%\WEB-INF"
+xcopy /y "%web_xml1%" "%temp1%\WEB-INF"
 
-xcopy /y "%work_dir%\webfournisseur\*.jsp" "%temp%"
+:: Copie des fichiers .jsp vers les dossiers temporaires
+xcopy /y "%work_dir%\webfournisseur\*.jsp" "%temp%\"
+xcopy /y "%work_dir%\web\*.jsp" "%temp1%\"
 
-xcopy "%work_dir%\webfournisseur\assets" "%temp%" /E /I /Y
+:: Copie des dossiers assets et forms vers les dossiers temporaires
+xcopy "%work_dir%\webfournisseur\assets" "%temp%\assets" /E /I /Y
+xcopy "%work_dir%\webfournisseur\forms" "%temp%\forms" /E /I /Y
+xcopy "%work_dir%\web\assets" "%temp1%\assets" /E /I /Y
+xcopy "%work_dir%\web\forms" "%temp1%\forms" /E /I /Y
 
-xcopy "%work_dir%\webfournisseur\forms" "%temp%" /E /I /Y
-
-xcopy /y "%config_xml%" "%temp%\WEB-INF\classes"
-
-:: Copie des fichiers .jar dans lib vers le dossier temporaire...
+:: Copie des fichiers .jar dans lib vers les dossiers temporaires
 xcopy /s /y "%lib%\*.jar" "%temp%\WEB-INF\lib"
-
-:: Copie de web.xml ver temp/WEB-INF...
-xcopy /y "%web_xml%" "%temp1%\WEB-INF"
-
-xcopy /y "%work_dir%\web\*.jsp" "%temp1%"
-
-xcopy "%work_dir%\web\assets" "%temp1%" /E /I /Y
-
-xcopy "%work_dir%\web\forms" "%temp1%" /E /I /Y
-
-xcopy /y "%config_xml%" "%temp1%\WEB-INF\classes"
-
-:: Copie des fichiers .jar dans lib vers le dossier temporaire...
 xcopy /s /y "%lib%\*.jar" "%temp1%\WEB-INF\lib"
 
-echo Compilation des fichiers.java...
-:: Compilation des fichiers.java dans src avec les options suivantes
-:: Note: Assurez-vous que le chemin vers le compilateur Java (javac) est correctement configuré dans votre variable d'environnement PATH.
-:: Créer une liste de tous les fichiers.java dans le répertoire src et ses sous-répertoires
+:: Compilation des fichiers .java
 dir /s /B "%src%\*.java" > sources.txt
 if not exist sources.txt (
-    echo Aucun fichier.java trouvé dans le répertoire src.
+    echo Aucun fichier .java trouvé dans le répertoire src.
     exit /b 1
 )
 
-:: Création d'un fichier temporaire pour stocker les chemins des fichiers.jar
+:: Création du classpath pour la compilation
 dir /s /B "%lib%\*.jar" > libs.txt
-
-:: Construire le classpath
 set "classpath="
 for /F "delims=" %%i in (libs.txt) do set "classpath=!classpath!.;%%i;"
 
@@ -85,45 +72,35 @@ if %errorlevel% neq 0 (
     exit /b 1
 )
 
-:: Exécuter la commande javac
+:: Exécution de la compilation
 javac -d "%temp%\WEB-INF\classes" -cp "%classpath%" @sources.txt
-
 javac -d "%temp1%\WEB-INF\classes" -cp "%classpath%" @sources.txt
 
-:: Supprimer les fichiers sources.txt et libs.txt après la compilation
+:: Nettoyage des fichiers temporaires de compilation
 del sources.txt
 del libs.txt
 
-:: Créer un fichier.war nommé [war_name].war à partir du dossier [temp] et son contenu dans le dossier [work_dir]
+:: Création des fichiers .war
 cd "%temp%"
 jar -cvf "%work_dir%\%war_name%.war" *
-cd %work_dir%
+cd "%work_dir%"
 
-:: Créer un fichier.war nommé [war_name].war à partir du dossier [temp] et son contenu dans le dossier [work_dir]
 cd "%temp1%"
 jar -cvf "%work_dir%\%war_name1%.war" *
 cd %work_dir%
 
-:: Effacer le fichier.war dans [web_apps] s'il existe
+:: Suppression et déploiement des fichiers .war
 if exist "%web_apps%\%war_name%.war" (
     del /f /q "%web_apps%\%war_name%.war"
 )
-
-:: Copier le fichier.war vers [web_apps]
 copy /y "%work_dir%\%war_name%.war" "%web_apps%"
-
 del /f /q "%work_dir%\%war_name%.war"
 
-:: Effacer le fichier.war dans [web_apps] s'il existe
 if exist "%web_apps%\%war_name1%.war" (
     del /f /q "%web_apps%\%war_name1%.war"
 )
-
-:: Copier le fichier.war vers [web_apps]
 copy /y "%work_dir%\%war_name1%.war" "%web_apps%"
-
 del /f /q "%work_dir%\%war_name1%.war"
 
 echo Déploiement terminé.
-
 pause
